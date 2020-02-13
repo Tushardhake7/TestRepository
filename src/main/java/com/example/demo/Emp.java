1413b6e7efd2abe4aef8b6e16a7537952725c02d
Tushar <tushar.dhake@yash.com>
2020-02-13 09:20:53 -0800

    Emp class created

---
+++ src/main/java/com/example/demo/Emp.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Emp {
+	int abc;
+	int pwr;
+
+}
--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -4,5 +4,20 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 public class User {
 	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+	
 
 }
